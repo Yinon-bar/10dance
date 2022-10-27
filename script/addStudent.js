a5db39f90412846f2b80b5ce703d8799de05a0d1
Yaron Veg <Yaronveg1@gmail.com>
2022-10-27 23:01:39 +0300

    change fetch to async await, lookup using t_z_id

--- script/addStudent.js
+++ script/addStudent.js
@@ -2,7 +2,7 @@ import { API_URL } from "/script/apiService.js";
 
 const declareEvents = () => {
   let id_form = document.querySelector("#id_form");
-  id_form.addEventListener("submit", (e) => {
+  id_form.addEventListener("submit", async (e) => {
     e.preventDefault();
     // TODO: לבדוק שגיאות באינפוטים של המשתמש
     let bodyData = {
@@ -11,50 +11,50 @@ const declareEvents = () => {
       last: document.querySelector("#id_last").value,
       // if_dikan: document.querySelector("#id_if_dikan").value,
     };
-    addApiReq(bodyData);
-    doApi();
-    window.location =
-      "../admin/print_page.html?name=" + bodyData.first + " " + bodyData.last;
+    const nameParam = bodyData.first + " " + bodyData.last;
+    const t_z_id = bodyData.t_z_id;
+    await addAttendeeToDB(bodyData);
+    const students = await getAllStudents();
+    await printAttendeeFromList(students, bodyData.t_z_id);
+    window.location = `../admin/print_page.html?name=${nameParam}&id=${t_z_id}`;
   });
 };
 
-const doApi = () => {
-  let url = API_URL + "/students_list.php";
-  fetch(url)
-    .then((resp) => resp.json())
-    .then((data) => {
-      printStudent(data);
-    });
+const getAllStudents = async () => {
+  try {
+    let url = API_URL + "/students_list.php";
+    const resp = await fetch(url);
+    const data = await resp.json();
+
+    return data;
+  } catch {}
 };
 
-const printStudent = (_ar) => {
-  let isHere = false;
-  for (const item of _ar) {
-    console.log(item);
-    isHere = true;
-    userName = console.log(isHere);
-    const printUser = function (arr) {
-      window.location = "../../Admin/print_page.html?id=" + arr.id;
-    };
-    printUser(item);
+const printUser = function (arg) {
+  window.location = "../admin/print_page.html?id=" + arg.id;
+};
+
+const printAttendeeFromList = async (list, t_z_id) => {
+  const attendee = list.find((attendee) => attendee.t_z_id === t_z_id);
+  if (attendee) {
+    printUser(attendee);
+  } else {
+    alert("Attendee missing");
   }
 };
 
-const addApiReq = (_bodyData) => {
-  let url = API_URL + "/add_students.php";
-  fetch(url, {
-    method: "POST",
-    body: JSON.stringify(_bodyData),
-    headers: { "content-type": "application/json" },
-  })
-    .then((resp) => resp.json())
-    .then((data) => {
-      if (data.status === "ok") {
-        alert("Stuednt added");
-      } else {
-        console.log(data);
-      }
+const addAttendeeToDB = async (_bodyData) => {
+  try {
+    let url = API_URL + "/add_students.php";
+    await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(_bodyData),
+      headers: { "content-type": "application/json" },
     });
+    alert("Stuednt added");
+  } catch {
+    throw new Error("Error adding attendee");
+  }
 };
 
 declareEvents();
