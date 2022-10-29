1e43f7c226b7940c6af07daed172bc69ceaef054
Yaron Veg <Yaronveg1@gmail.com>
2022-10-29 23:21:34 +0300

    split print to get attendee and print navigation

--- script/utils/print.js
+++ script/utils/print.js
@@ -1,11 +1,14 @@
-export const printAttendeeFromList = async (list, t_z_id, postPrintURL) => {
-  const attendee = list.find((attendee) => attendee.t_z_id === t_z_id);
-  if (attendee) {
-    window.location =
-      "../admin/print_page.html?id=" +
-      t_z_id +
-      `&post-print-url=${postPrintURL}`;
-  } else {
-    alert("Attendee missing");
-  }
+import { getAllStudents } from "./students.js";
+
+export const getAttendeeFromDB = async (t_z_id) => {
+  try {
+    const attendees = await getAllStudents();
+    const atteendee = attendees.find((attendee) => attendee.t_z_id === t_z_id);
+    return atteendee;
+  } catch {}
+};
+
+export const printAttendee = async (t_z_id, postPrintURL) => {
+  window.location =
+    "../admin/print_page.html?id=" + t_z_id + `&post-print-url=${postPrintURL}`;
 };
