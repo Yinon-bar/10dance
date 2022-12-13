017d08f76bf2544f8c8e801163b325273d5a55ef
Yaron Veg <Yaronveg1@gmail.com>
2022-12-13 22:27:50 +0200

    update connection to new db

--- php/connect.php
+++ php/connect.php
@@ -4,7 +4,7 @@ header('Content-Type: text/html; charset=utf-8');
 $dbhost = "localhost:3306";
 $dbuser = "zerdance_yinon";
 $dbpass = "dance053508384";
-$dbname = "zerdance_hebrew_uni";
+$dbname = "zerdance_dec_geo";
 
 
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
